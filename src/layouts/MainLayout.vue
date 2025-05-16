2488e008b089bc17383ff8e93dbaab3275c783e2
Kikisito <bekikisito@gmail.com>
2025-05-16 22:35:24 +0200

    Mostrar aviso cuando el dispositivo no tiene conexión a Internet (layout Usuario)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,3 +1,56 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useAuthStore } from 'src/stores/AuthStore'
+import { useUserStore } from 'src/stores/UserStore'
+import type { ConnectionStatus } from '@capacitor/network'
+import { Network } from '@capacitor/network'
+
+const authStore = useAuthStore()
+const userStore = useUserStore()
+
+const drawer = ref(false)
+const miniState = ref(true)
+
+const deviceHasNetwork = ref<ConnectionStatus>()
+
+function toggleDrawer() {
+  drawer.value = !drawer.value
+}
+
+const menuItems = [
+  { label: 'Inicio', icon: 'home', path: '/home', show: true },
+  { label: 'Mis citas', icon: 'view_agenda', path: '/appointments', show: true },
+  { label: 'Mensajería', icon: 'chat', path: '/chats', show: true },
+  { label: 'Informes', icon: 'description', path: '/reports', show: true },
+  { label: 'Recetas', icon: 'receipt', path: '/recipes', show: true },
+  { label: 'Pruebas y Analíticas', icon: 'science', path: '/tests', show: true, separator: true },
+  {
+    label: 'Administración',
+    icon: 'admin_panel_settings',
+    path: '/admin',
+    show: userStore.hasRole('ADMIN'),
+  },
+  {
+    label: 'Área profesional',
+    icon: 'medical_information',
+    path: '/professional',
+    show: userStore.hasRole('PROFESSIONAL'),
+  },
+  { label: 'Mi perfil', icon: 'person', path: '/profile', show: true },
+  { label: 'Ajustes', icon: 'settings', path: '/settings', show: true },
+  { label: 'Cerrar sesión', icon: 'logout', show: true, action: () => authStore.logout() },
+]
+
+onMounted(async () => {
+  const status = await Network.getStatus()
+  deviceHasNetwork.value = status
+
+  Network.addListener('networkStatusChange', (status) => {
+    deviceHasNetwork.value = status
+  })
+})
+</script>
+
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated>
@@ -42,47 +95,21 @@
     </q-drawer>
 
     <q-page-container>
+      <q-banner
+        v-if="deviceHasNetwork?.connected === false"
+        class="bg-red-5 text-white q-ma-md"
+        inline-actions
+        dense
+        rounded
+      >
+        <template v-slot:avatar>
+          <q-icon name="wifi_off" size="2em" class="q-mr-sm" />
+        </template>
+        <div class="text-h6">No estás conectado a Internet.</div>
+        Algunas partes de la aplicación no estarán disponibles mientras no te conectes.
+      </q-banner>
+
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import { useAuthStore } from 'src/stores/AuthStore'
-import { useUserStore } from 'src/stores/UserStore'
-
-const authStore = useAuthStore()
-const userStore = useUserStore()
-
-const drawer = ref(false)
-const miniState = ref(true)
-
-function toggleDrawer() {
-  drawer.value = !drawer.value
-}
-
-const menuItems = [
-  { label: 'Inicio', icon: 'home', path: '/home', show: true },
-  { label: 'Mis citas', icon: 'view_agenda', path: '/appointments', show: true },
-  { label: 'Mensajería', icon: 'chat', path: '/chats', show: true },
-  { label: 'Informes', icon: 'description', path: '/reports', show: true },
-  { label: 'Recetas', icon: 'receipt', path: '/recipes', show: true },
-  { label: 'Pruebas y Analíticas', icon: 'science', path: '/tests', show: true, separator: true },
-  {
-    label: 'Administración',
-    icon: 'admin_panel_settings',
-    path: '/admin',
-    show: userStore.hasRole('ADMIN'),
-  },
-  {
-    label: 'Área profesional',
-    icon: 'medical_information',
-    path: '/professional',
-    show: userStore.hasRole('PROFESSIONAL'),
-  },
-  { label: 'Mi perfil', icon: 'person', path: '/profile', show: true },
-  { label: 'Ajustes', icon: 'settings', path: '/settings', show: true },
-  { label: 'Cerrar sesión', icon: 'logout', show: true, action: () => authStore.logout() },
-]
-</script>
